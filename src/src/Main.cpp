f5f95b33aef81ed0386457644ea0576c71adcc25
georgemoralis <giorgosmrls@gmail.com>
2022-05-31 18:49:26 +0300

    Added menu with memviewer

--- src/src/Main.cpp
+++ src/src/Main.cpp
@@ -137,7 +137,8 @@ int main(int, char**) {
 
         // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code
         // to learn more about Dear ImGui!).
-        if (show_demo_window) ImGui::ShowDemoWindow(&show_demo_window);
+        /* if (show_demo_window)
+            ImGui::ShowDemoWindow(&show_demo_window);
 
         // 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
         {
@@ -172,8 +173,16 @@ int main(int, char**) {
             ImGui::Text("Hello from another window!");
             if (ImGui::Button("Close Me")) show_another_window = false;
             ImGui::End();
-        }
+        }*/
         
+        if (ImGui::BeginMainMenuBar()) {
+
+            if (ImGui::BeginMenu("Debug")) {
+                ImGui::MenuItem("Memory Viewer", "", &show_memory);
+                ImGui::EndMenu();
+            }
+            ImGui::EndMainMenuBar();       
+        }
         if (show_memory) {
             debug::memoryViewer::debug_window_memory(show_memory);
         }
